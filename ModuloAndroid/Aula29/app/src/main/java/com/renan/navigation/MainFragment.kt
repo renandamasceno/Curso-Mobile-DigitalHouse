ae9f73ad15a1b1e88d6691b3cc613a9deb6dae83
Renan <jrferreirad@gmail.com>
2020-11-08 18:53:22 -0300

    Adicionando Aula 29 - android jetpack

---
+++ ModuloAndroid/Aula29/app/src/main/java/com/renan/navigation/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.renan.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<Button>(R.id.btn).setOnClickListener {
+            navController.navigate(R.id.blankFragment)
+        }
+        view.findViewById<Button>(R.id.btnAzul).setOnClickListener {
+            navController.navigate(R.id.azulFragment)
+        }
+    }
+
+}
